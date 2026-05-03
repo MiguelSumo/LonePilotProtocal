79facc99de395d9d80956db49fcc894fb679c4a8
Cole-Gibeau <156457369+Cole-Gibeau@users.noreply.github.com>
2026-05-02 20:40:13 -0500

    Implemented all of the new prefabs into the existing enemyType enum and the enemyfactory

--- Assets/Scripts/Enemy/EnemyFactory.cs
+++ Assets/Scripts/Enemy/EnemyFactory.cs
@@ -7,6 +7,11 @@ public class EnemyFactory : MonoBehaviour
 {
     public Enemy BlueEnemyPreFab;
     public Enemy RedEnemyPreFab;
+    public Enemy AbyssalTwoa;
+    public Enemy AbyssalSixc;
+    public Enemy AbyssalFiveb;
+    public Enemy AbyssalThreec;
+
 
     public Enemy CreateEnemy(EnemyType type, Vector3 position, WaveManager waveManager)
     {
@@ -15,19 +20,30 @@ public class EnemyFactory : MonoBehaviour
         switch (type)
         {
             case EnemyType.BasicBlue:
-                prefabToSpawn = BlueEnemyPreFab;
-                break;
-            case EnemyType.BasicRed:
-                prefabToSpawn = RedEnemyPreFab;
-                break;
-
             case EnemyType.ZigZagBlue:
                 prefabToSpawn = BlueEnemyPreFab;
                 break;
-
+            case EnemyType.BasicRed:
             case EnemyType.ZigZagRed:
                 prefabToSpawn = RedEnemyPreFab;
                 break;
+            case EnemyType.Basic2a:
+            case EnemyType.ZigZag2a:
+                prefabToSpawn = AbyssalTwoa;
+                break;
+            case EnemyType.Basic6c:
+            case EnemyType.ZigZag6c:
+                prefabToSpawn = AbyssalSixc;
+                break;
+            case EnemyType.Basic5b:
+            case EnemyType.ZigZag5b:
+                prefabToSpawn = AbyssalFiveb;
+                break;
+            case EnemyType.Basic3c:
+            case EnemyType.ZigZag3c:
+                prefabToSpawn = AbyssalThreec;
+                break;
+
         }
 
         Enemy enemy = Instantiate(prefabToSpawn, position, Quaternion.identity);
@@ -37,11 +53,19 @@ public class EnemyFactory : MonoBehaviour
         {
             case EnemyType.BasicBlue:
             case EnemyType.BasicRed:
+            case EnemyType.Basic2a:
+            case EnemyType.Basic3c:
+            case EnemyType.Basic5b:
+            case EnemyType.Basic6c:
                 enemy.SetTrackingType(Enemy.TrackingType.Simple);
                 break;
 
             case EnemyType.ZigZagBlue:
             case EnemyType.ZigZagRed:
+            case EnemyType.ZigZag2a:
+            case EnemyType.ZigZag3c:
+            case EnemyType.ZigZag5b:
+            case EnemyType.ZigZag6c:
                 enemy.SetTrackingType(Enemy.TrackingType.ZigZag);
                 break;
         }
